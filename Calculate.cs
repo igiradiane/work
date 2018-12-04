488b9d8a199323dae95f4665d7130150e49154a6
iraduFulgence <iradufulgence@hotmail.com>
2018-12-04 12:58:34 +0200

    added c# assignment

---
+++ Calculate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Calculate
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Division(double num1, double num2)
+        {
+            
+            return num1/ num2;
+        }
+        public double Multplication(double num1, double num2)
+
+        {
+            string cool;
+            int num,k;
+            Console.WriteLine("do you wish a multiplication table of a any number?");
+            cool = Console.ReadLine();
+            if(cool=="Y"|| cool=="Y")
+            {
+                Console.WriteLine("Enter any number");
+                num = Convert.ToInt32(Console.ReadLine());
+                for(k=1;k<=10;k++)
+                {
+                    Console.WriteLine("the multiplicatio table of number {0}x{1} is ={2}", num, k, num * k);
+                }
+
+
+
+            }
+            else if(cool=="N"||cool=="N")
+            {
+                
+            }
+            return num1 * num2;
+
+        }
+        public double subtraction(double num1, double num2)
+        {
+            
+            if(num1==0)
+            {
+                throw new Exception("num1 should not be zero");
+
+            }
+            else
+            {
+                return num1 - num2;
+            }
+        }
+
+    }
+
+}
---
+++ Cars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Cars
+    {
+        public int horsepower;
+        private string Make;
+        public string color;
+        public string year;
+        public int speed;
+        public string make { get; set; }
+        public int Accelerate(int initSpeed)
+        {
+            return speed = initSpeed + 5;
+        }
+        public int Accerelate(int initspeed)
+        {
+            return speed = initspeed + 10;
+        }
+    }
+    
+}
