96a4069c3e9036e7589ae8795852e3ee50761ac1
Diane <igiradiane@gmail.com>
2018-12-04 13:34:05 +0300

    added calculatorApp

---
+++ Calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Calculate
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Division(double num1, double num2)
+        {
+            
+            return num1/ num2;
+        }
+        public double Multplication(double num1, double num2)
+
+        {
+            
+           
+            return num1*num2;
+        }
+        public double subtraction(double num1, double num2)
+        {
+            
+            if(num1==0)
+            {
+                throw new Exception("num1 should not be zero");
+
+            }
+            else
+            {
+                return num1 - num2;
+            }
+        }
+
+    }
+
+}
